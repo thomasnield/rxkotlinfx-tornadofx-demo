461d8e9d532d57e6c4f1648743c3bbde0a1f17d9
Matthew Turnblom <mbturnblom@xactware.com>
2016-07-26 17:05:56 -0600

    Cleaned up with new App call

--- src/main/kotlin/app/MyApp.kt
+++ src/main/kotlin/app/MyApp.kt
@@ -2,16 +2,11 @@ package app
 
 import javafx.scene.image.Image
 import tornadofx.App
-import tornadofx.FX
 import tornadofx.addStageIcon
-import tornadofx.importStylesheet
 import view.MainView
 
-class MyApp : App() {
-    override val primaryView = MainView::class
-
+class MyApp : App(MainView::class, Styles::class) {
     init {
-        importStylesheet(Styles::class)
         addStageIcon(Image("/app/tornado-fx-logo.png"))
     }
 }
